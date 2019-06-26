606248033de05ee59917b2e06390ac4a23cd7fb7
Shandler Mason <samason1@fb.com>
2019-06-26 11:29:47 -0700

    Initial Commit

---
+++ Flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Flix
+//
+//  Created by samason1 on 6/26/19.
+//  Copyright © 2019 samason1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Flix/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Flix
+//
+//  Created by samason1 on 6/26/19.
+//  Copyright © 2019 samason1. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
